cc1ccb1aab7ef072343c0c271858564271dc11a1
wasabhi <abhinav.keswani@trineo.co.nz>
2020-04-04 18:36:46 -0600

    PDF, txt and JSON versions of film listing.

---
+++ docs/transform.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sed -e 's/^/{"title":"/;s/ (/", "year":"/;s/\(20[0-9][0-9]\), /\1", "lengthInMin":"/;s/ min) /", "description":"/;s/ http/", "url":"http/;s/$/"},/' banff.txt > banff.json
