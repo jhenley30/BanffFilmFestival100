32ff8ed5dc5ec79e9c40b89e30dc1de1f2be8316
wasabhi <abhinav.keswani@trineo.co.nz>
2020-04-04 20:41:45 -0600

    Rename files.

--- docs/transform.sh
+++ docs/transform.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sed -e 's/^/{"title":"/;s/ (/", "year":"/;s/\(20[0-9][0-9]\), /\1", "lengthInMin":"/;s/ min) /", "description":"/;s/ http/", "url":"http/;s/$/", "tags":""},/' banff.txt > banff.json
+sed -e 's/^/{"title":"/;s/ (/", "year":"/;s/\(20[0-9][0-9]\), /\1", "lengthInMin":"/;s/ min) /", "description":"/;s/ http/", "url":"http/;s/$/", "tags":""},/' movies.txt > movies.json
